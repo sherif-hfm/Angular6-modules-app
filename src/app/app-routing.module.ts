c50e1a7b8597e6cf7daed3fd374432c10421bfea
Sherif Mohamed <sherif_hfm@yahoo.com>
2019-01-11 15:22:32 -0600

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Part1GardGuard } from './part1-gard.guard';
+import { Comp1Component } from './part2/comp1/comp1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component:HomeComponent,pathMatch: 'full'} ,  
+  {path : 'part1', loadChildren:'./part1/part1.module#Part1Module',canLoad:[Part1GardGuard]} ,  
+  {path : 'part2/comp1', component:Comp1Component,pathMatch: 'full'}  , 
+ /* {path : 'signout', component:SigninComponent,data:{msg:'signout'},pathMatch: 'full'}  , 
+  {path : 'servers', component:ServersComponent,pathMatch: 'full',canActivate:[ServersGuard]} */
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})], /* preloadingStrategy to load all lazy module after application start */
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Part1Module } from './part1/part1.module';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './Shared/shared.module';
+import { Part2Module } from './part2/part2.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule /* this module for root module only */ ,
+    SharedModule,
+    //Part1Module /* all feature modules imported before AppRouting */,
+    Part2Module,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
